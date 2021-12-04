8f2cd499049a58e9d68aa6b9103479d85ba1cb6a
Sam Hollyer <sam.hollyer@tensure.io>
2021-12-04 13:28:13 -0600

    add project detail template.set up to use html and img in each

---
+++ src/templates/ProjectDetails.jsx
@@ -0,0 +1,19 @@
+import Img from 'gatsby-image'
+import React from 'react'
+import Layouts from '../components/Layouts'
+import * as styles from '../styles/project-details.module.less'
+
+export default function ProjectDetails() {
+  return (
+    <Layouts>
+      <div className={styles.details}>
+        <h2>title</h2>
+        <h3>stack</h3>
+        <div className={styles.featured}>
+          <Img fluid />
+        </div>
+        <div className={styles.html} dangerouslySetInnerHTML />
+      </div>
+    </Layouts>
+  )
+}
