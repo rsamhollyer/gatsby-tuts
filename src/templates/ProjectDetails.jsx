9d38d6dd304793b9e1097558195019e764ba131c
Sam Hollyer <sam.hollyer@tensure.io>
2021-12-04 13:48:31 -0600

    hook up details page with query data

--- src/templates/ProjectDetails.jsx
+++ src/templates/ProjectDetails.jsx
@@ -1,22 +1,47 @@
+import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import React from 'react'
 import Layouts from '../components/Layouts'
 import * as styles from '../styles/project-details.module.less'
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+  const { html } = data.markdownRemark
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter
   return (
     <Layouts>
       <div className={styles.details}>
-        <h2>title</h2>
-        <h3>stack</h3>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
         <div className={styles.featured}>
-          <Img fluid />
+          <Img fluid={featuredImg.childImageSharp.fluid} />
         </div>
         <div
           className={styles.html}
-          //   dangerouslySetInnerHTML={{ __html: html }}
+          dangerouslySetInnerHTML={{ __html: html }}
         />
       </div>
     </Layouts>
   )
 }
+
+// Import Query
+
+export const query = graphql`
+  query Details($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        slug
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+      html
+    }
+  }
+`
